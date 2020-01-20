30fa7996e76f0b58b4779650a869032ccdec0d9c
James Biederbeck <james@jamesbiederbeck.com>
2020-01-20 02:49:58 -0800

    Wrapped function in flask as microservice

--- devicetable.py
+++ devicetable.py
@@ -2,6 +2,11 @@ from requests_html import HTMLSession
 import bs4
 from pprint import pprint
 
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
 def getHostNamesFromRouter():
     session = HTMLSession()
     r = session.get('https://10.0.0.1', verify=False)
@@ -13,7 +18,7 @@ def getHostNamesFromRouter():
         hosts.append(host)
     print(f"Found {len(hosts)} hosts")
     pprint(hosts)
-    return hosts
+    return jsonify(hosts)
 
 if __name__ == "__main__":
-    getHostNamesFromRouter()
+    app.run(debug=True)

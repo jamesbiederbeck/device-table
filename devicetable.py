c603bd1626e72414d7845dea8a2e1e9ebcfa03a0
James Biederbeck <james@jamesbiederbeck.com>
2020-01-22 20:50:35 -0800

    renamed/added variables to improve readability

--- devicetable.py
+++ devicetable.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    getHostNamesFromRouter()
+    hosts = getHostNamesFromRouter()
     #only return json string if we're running in flask
     if "scan" not in sys.argv:
         return jsonify(hosts)
@@ -23,9 +23,9 @@ def main():
 def getHostNamesFromRouter():
     session = HTMLSession()
     r = session.get('https://10.0.0.1', verify=False)
-    foo = r.html.xpath('//*[@id="internet-usage"]/table')[0]
+    data = r.html.xpath('//*[@id="internet-usage"]/table')[0]
     hosts = []
-    for row in foo.xpath("//tr")[1:]: #skip header
+    for row in data.xpath("//tr")[1:]: #skip header
         host = {}
         host["host_name"],host["mac_address"],host["connection"] = row.text.splitlines()
         hosts.append(host)
